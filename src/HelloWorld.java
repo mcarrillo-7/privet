d8f3a968121a58e1bfe687c52818991cfd6284ea
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2019-06-14 09:56:57 +0200

    PRIMERA PRUEBA

---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		String nombre = "Ander";
+		
+		System.out.println("Hello " + nombre);
+		
+		/*Person p1 = new Person(); //Instanciar un objeto, crear un objeto tipo Person
+		p1.setNombre("Manolo");*/
+		
+		//System.out.println(p1.saludar());
+		
+		/*Perro perro1 = new Perro();
+		//perro1.setNombre("Leia");
+		perro1.setEdad(-3);
+		//perro1.setRaza("bichon maltes");
+		perro1.setVacunado(true);
+		System.out.println(perro1.toString());
+		
+		
+		PerroPresa buba = new PerroPresa();
+		buba.setNombre("buba");
+		buba.atacar();*/
+	}
+
+}
